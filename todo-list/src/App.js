e938de09a18358cf810162643cdd50acf177f6be
Tom <tomlazore@gmail.com>
2025-06-16 16:30:47 -0400

    setText to the target value of the event on the input field

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [toDo, setTodo] = useState([]);
+  const [text, setText] = useState('');
+
+  const handleInputChange = (e) => {
+    console.log(e.target.value)
+    setText(e.target.value)
+  }
   return (
     <div className="App">
-      hello
+      <form>
+        <input
+          placeholder="Add Todo"
+          type="text"
+          value={text}
+          onChange={handleInputChange}
+        />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
