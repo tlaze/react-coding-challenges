837ad55f6fbabdc17bf30829c2d47c811810976e
Tom <tomlazore@gmail.com>
2025-06-16 16:40:59 -0400

    jsx maps through list of todos and displays them in a list

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -9,17 +9,42 @@ function App() {
     console.log(e.target.value)
     setText(e.target.value)
   }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    if(text.trim() === '') return
+    console.log("button clicked")
+
+    const newTodo = {
+      id: Date.now(),
+      text: text,
+      completed: false
+    }
+
+    setTodo([...toDo, newTodo]);
+    setText('')
+  }
+
   return (
     <div className="App">
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           placeholder="Add Todo"
           type="text"
           value={text}
           onChange={handleInputChange}
         />
-        <button>Submit</button>
+        <button
+        >Submit</button>
       </form>
+      <ul>
+        {toDo.map((todo) => (
+          <li key={todo.id}>
+            {todo.text}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
