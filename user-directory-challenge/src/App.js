186f471cd2db0f3e487a7f982a8bef76f90ca598
Tom <tomlazore@gmail.com>
2025-06-16 15:09:38 -0400

    created useEffect hook with Javascript promises and asyncronous promises to demonstrate different methods making api requests in useEffect hook

--- user-directory-challenge/src/App.js
+++ user-directory-challenge/src/App.js
@@ -1,6 +1,56 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const filteredUsers = users.filter(user =>
+    user.name.toLowerCase().includes(searchTerm.toLowerCase())
+  )
+
+  // //Load users via Javascript promises
+  // useEffect(() => {
+  //   fetch('https://jsonplaceholder.typicode.com/users')
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       setUsers(data);
+  //       console.log(data);
+  //       setIsLoading(false);
+  //     })
+  //     .catch(err => {
+  //       setError('Failed to load users.');
+  //       setIsLoading(false)
+  //     })
+  // }, [])
+
+  //Load users via asyncronous promises (my preference)
+  //This useEffect hook will asyncronously fetch users data from the api and set
+  //that data into the "users" state variable. Error handling implemented to
+  //catch any errors during fetch request.
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+          const response = await fetch('https://jsonplaceholder.typicode.com/users');
+          const data = await response.json();
+          console.log(data);
+          setUsers(data);
+      }
+      catch (err) {
+        setError('Failed to load users.');
+      }
+      finally {
+        setIsLoading(false);
+      }
+    }
+    fetchUsers()
+    },[]);
+
+
   return (
     <div className="App">
       <h1>User Directory</h1>
