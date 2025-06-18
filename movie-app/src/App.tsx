b7cb7401d34df38272118c5535b15db7aef506a9
Tom <tomlazore@gmail.com>
2025-06-17 20:50:33 -0400

    created an api key for tmdb and stored it in .env. fetched list of movies in the console

--- movie-app/src/App.tsx
+++ movie-app/src/App.tsx
@@ -1,14 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import MovieGrid from './components/MovieGrid';
 import FilterBar from './components/FilterBar';
 import Moviecard from './components/MovieCard';
-import './App.css'
+
 
 function App() {
 
   const [movies, setMovies] = useState([])
   const [isLoading, setIsLoading] = useState(true)
   const [error, setError] = useState('')
+
+  const api_key = import.meta.env.VITE_API_KEY;
+  console.log(api_key)
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=1`);
+      const data = await response.json();
+      console.log(data)
+    }
+    fetchMovies();
+  },[])
 
   return (
     <>
